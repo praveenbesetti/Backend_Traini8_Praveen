705064ce0d63117dbeeb4121864d932599b3d764
praveenbesetti <praveenbesetti0@gmail.com>
2024-04-27 16:30:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TrainingCenter from './components/registationPage';
+import { Route, Routes } from 'react-router-dom';
+import CodePage from './components/code';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<TrainingCenter />}> </Route>
+        <Route path="/code/:generatedString" element={<CodePage />} />
+      
+     </Routes> 
     </div>
   );
 }
